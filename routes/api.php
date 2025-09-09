5d8468cf52ca3d485a748e6cd321bcee210862a8
Satya Uttami <satyautta@gmail.com>
2025-09-09 16:33:10 +0800

    commit

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\Api\BlogController;
+use App\Http\Controllers\API\BlogController;
 
 Route::prefix('v1/auth')->group(function () {
     Route::post("register", [AuthController::class, "register"]);
